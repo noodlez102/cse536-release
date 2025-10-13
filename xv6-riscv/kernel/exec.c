b54a09b080b4ded44cc14a0923ca2c270dffdba9
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-13 16:05:40 -0700

    used internally defined strncmp

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -6,7 +6,6 @@
 #include "proc.h"
 #include "defs.h"
 #include "elf.h"
-#include <string.h>
 
 // static 
 int loadseg(pde_t *, uint64, struct inode *, uint, uint);
@@ -34,7 +33,7 @@ exec(char *path, char **argv)
   struct proc *p = myproc();
 
   /* CSE 536: (2.1) Check on-demand status. */
-  if (strcmp(path, "init") == 0 ||strcmp(path, "sh") == 0 ) {
+  if (strncmp(path, "init",4) == 0 ||strncmp(path, "sh",2) == 0 ) {
     p->ondemand = false;
   } else {
     p->ondemand = true;
