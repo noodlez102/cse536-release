1d5bc44e420ece03ce341acac3abbc78c9592347
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-14 14:06:09 -0700

    redirected to page fault handler

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -67,6 +67,9 @@ usertrap(void)
     intr_on();
 
     syscall();
+  } else if(r_scause()==0xC){
+    print("Sent to page fault handler");
+    page_fault_handler();
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
