e60a783194d528239b7b690e99f3bc7c96c5d7ac
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-14 19:28:52 -0700

    pg table

--- xv6-riscv/kernel/pfault.c
+++ xv6-riscv/kernel/pfault.c
@@ -141,7 +141,6 @@ void page_fault_handler(void)
             print_load_seg(faulting_addr,ph.off, ph.memsz);
             iunlockput(ip);
             end_op();
-            proc_freepagetable(pagetable, sz);
             goto out;
         }
     }
