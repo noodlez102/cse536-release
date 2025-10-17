7ba8f8c1954b6eb49220f7c34a4dfa47db7400fb
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 16:56:57 -0700

    fixing fifo

--- xv6-riscv/kernel/pfault.c
+++ xv6-riscv/kernel/pfault.c
@@ -51,7 +51,7 @@ void evict_page_to_disk(struct proc* p) {
     /* Find victim page using FIFO. */
     struct heap_tracker_t *victim = &p->heap_tracker[0];
     for(int i =1; i<MAXHEAP;i++){
-        if(p->heap_tracker[i].last_load_time < victim->last_load_time){
+        if((p->heap_tracker[i].last_load_time < victim->last_load_time) && (victim->startblock==0)){
             victim= &p->heap_tracker[i];
         }
     }
