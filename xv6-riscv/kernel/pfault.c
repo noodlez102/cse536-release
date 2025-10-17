89eb273388296cbdc981f4749b33351c653443ad
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 16:34:14 -0700

    curiosity

--- xv6-riscv/kernel/pfault.c
+++ xv6-riscv/kernel/pfault.c
@@ -56,7 +56,7 @@ void evict_page_to_disk(struct proc* p) {
         }
     }
     /* Print statement. */
-    print_evict_page(victim->addr, blockno);
+    print_evict_page(PGROUNDDOWN(victim->addr), blockno);
     /* Read memory from the user to kernel memory first. */
     char *kernel_page = kalloc();
     if (copyin(p->pagetable, kernel_page, victim->addr, PGSIZE) < 0) {
