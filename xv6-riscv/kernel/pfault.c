e52bc07e877c74caecb4485b07c5509ac48e3ff5
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 21:29:17 -0700

    aight

--- xv6-riscv/kernel/pfault.c
+++ xv6-riscv/kernel/pfault.c
@@ -152,10 +152,10 @@ void page_fault_handler(void)
     print_page_fault(p->name, faulting_addr);
 
     //if it's copy on write
-    // if(p->cow_enabled){
-    //     copy_on_write();
-    //     goto out;
-    // }
+    if(p->cow_enabled && ((r_scause() & 0xF) == 0xD)){
+        copy_on_write();
+        goto out;
+    }
 
     /* Check if the fault address is a heap page. Use p->heap_tracker */
     for(int i=0; i<MAXHEAP;i++){
