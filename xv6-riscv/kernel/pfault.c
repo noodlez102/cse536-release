fa825aecbe7f798edaa9f209f07c8a36178c0399
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 18:09:45 -0700

    deleted unessecary things

--- xv6-riscv/kernel/pfault.c
+++ xv6-riscv/kernel/pfault.c
@@ -118,12 +118,7 @@ void retrieve_page_from_disk(struct proc* p, uint64 uvaddr) {
     }
 
     /* Copy from temp kernel page to uvaddr (use copyout) */
-    char *user_page = kalloc();
-    if (mappages(p->pagetable, uvaddr, PGSIZE, (uint64)kernel_page, PTE_W | PTE_U | PTE_R) < 0) {
-        printf("retrieve_page_from_disk: mappages failed\n");
-        kfree(kernel_page);
-        return;
-    }
+
     if (copyout(p->pagetable, uvaddr, kernel_page, PGSIZE) < 0)
         printf("retrieve_page_from_disk: copyout failed\n");
     retrieval->loaded=1;
@@ -131,7 +126,6 @@ void retrieve_page_from_disk(struct proc* p, uint64 uvaddr) {
     retrieval->startblock=-1;
 
     kfree(kernel_page);
-    kfree(user_page);
 
 }
 
