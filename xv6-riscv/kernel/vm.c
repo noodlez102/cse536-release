13487bacda4857d208e31769f6b039ad28ceee2e
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 23:59:57 -0700

    ok

--- xv6-riscv/kernel/vm.c
+++ xv6-riscv/kernel/vm.c
@@ -195,11 +195,15 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
       /* CSE 536: (2.6.1) Freeing Process Memory */
       // Make sure that the shared pages, belonging to a CoW group, are not freed 
       printf("before ref\n");
-      int ref_count = get_cow_group_count(p->cow_group);
-      printf("after ref\n");
-      if(ref_count == 1 || !p->cow_enabled) {
+      if(p->cow_enabled){
+        int ref_count = get_cow_group_count(p->cow_group);
+        if(ref_count==1){
+          kfree((void*)pa);
+        }
+      }else{
         kfree((void*)pa);
       }
+      
     }
     *pte = 0;
   }
