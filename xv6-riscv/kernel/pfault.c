06492fac41797be704d6f9ee67e761b5dd2a40de
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 22:27:20 -0700

    debugging

--- xv6-riscv/kernel/pfault.c
+++ xv6-riscv/kernel/pfault.c
@@ -153,7 +153,7 @@ void page_fault_handler(void)
 
     //if it's copy on write
     uint64 scause = r_scause();
-    printf("scause value: 0x%lx, cow_enabled: %d\n", scause, p->cow_enabled);
+    printf("scause value: 0x%x, cow_enabled: %d\n", scause, p->cow_enabled);
     if(p->cow_enabled && (r_scause() == 0xD || r_scause() == 0xF)){
         printf("gotten to copy on write\n");
         copy_on_write();
