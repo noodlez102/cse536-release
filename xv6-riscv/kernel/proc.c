16f3ee3f135129c657dd9c32f7058e230d4ceaa1
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 23:29:24 -0700

    more fixes

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -320,7 +320,7 @@ fork(int cow_enabled)
   /* CSE 536: (3.1) Modify fork() to handle CoW */
   // Currently fork() does not handle the case for when CoW is enable
   // You will have to implement the same
-  if(cow_enabled){
+  if(cow_enabled|| p->cow_enabled){
     // Set the appropriate metadata to track a CoW group
     int group_id;
     if(p->cow_enabled && p->cow_group != -1) {
