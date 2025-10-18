5ccc66d17235dea9ece418405587fbbb60ad71f4
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 23:32:27 -0700

    doind the discussion

--- xv6-riscv/kernel/vm.c
+++ xv6-riscv/kernel/vm.c
@@ -158,7 +158,7 @@ mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm)
     if((pte = walk(pagetable, a, 1)) == 0)
       return -1;
     if(*pte & PTE_V)
-      panic("mappages: remap");
+      //panic("mappages: remap");
     *pte = PA2PTE(pa) | perm | PTE_V;
     if(a == last)
       break;
