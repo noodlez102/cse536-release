2dcda8c70224b70ccfb716d4a9fc7d304778d86c
Arthur Hua <arthur.hua.102@gmail.com>
2025-10-17 23:18:48 -0700

    more debugfixing

--- xv6-riscv/kernel/cow.c
+++ xv6-riscv/kernel/cow.c
@@ -177,7 +177,7 @@ void copy_on_write() {
         return;
     }
 
-    print_copy_on_write(p, faulting_addr);
+    print_copy_on_write(p, new_mem);
     // Copy contents from the shared page to the new page
 
     // Map the new page in the faulting process's page table with write permissions
