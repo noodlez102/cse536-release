bcbd63606707c56d563c05175a6afb0b444f71d3
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-01 01:08:48 -0700

    proc_mapstacks

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -37,24 +37,21 @@ proc_mapstacks(pagetable_t kpgtbl)
 {
   // CSE 536: (Task 2.1.1) - Allocate and map MAXTHREADS kernel stacks for each process according to the instructions
   struct proc *p;
-  int tid;
-  
-  for(p = proc; p < &proc[NPROC]; p++) {
-    for(tid = 0; tid < MAXTHREADS; tid++) {
-      uint64 top = KSTACK((p - proc), tid);
-      uint64 stack_va = top + PGSIZE;
+  int pid, tid;
+  uint64 pa;
 
-      char *mem = kalloc();
-      if(mem == 0)
-        panic("kalloc for kstack failed");
-
-      memset(mem, 0, PGSIZE);
-      if(mappages(kpgtbl, stack_va, PGSIZE, (uint64)mem, PTE_R | PTE_W) != 0){
-          panic("proc_mapstacks: map failed");
-        }
+  for (pid = 0; pid < NPROC; pid++) {
+    for (tid = 0; tid < MAXTHREADS; tid++) {
+      pa = (uint64)kalloc();
+      if (pa == 0) {
+        panic("proc_mapstacks: kalloc out of memory while allocating kernel stacks");
+      }
+      memset((void*)pa, 0, PGSIZE);
+      if (mappages(kpgtbl, KSTACK(pid, tid), PGSIZE, pa, PTE_R | PTE_W) != 0) {
+        panic("proc_mapstacks: mappages failed");
       }
     }
-    printf("passed proc_mapstacks\n");
+  }
 }
 
 // initialize the proc table.
