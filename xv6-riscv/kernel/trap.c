654be21138f4244fc62b0033552a03e754977121
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-06 11:17:02 -0700

    passing tid to s11?

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -129,7 +129,7 @@ usertrapret(void)
   
   // CSE 536: (Task 2.1.2) - Pass the address of the current thread's TRAPFRAME in s11
   mythread()->trapframe->s11 = TRAPFRAME(mythread()->tid);
-  asm volatile("mv s11, %0" :: "r"(TRAPFRAME(mythread()->tid)) : "s11");
+  asm volatile("mv s11, %0" :: "r"(mythread()->tid) : "s11");
   ((void (*)(uint64))trampoline_userret)(satp);
 }
 
