38d3bd381e618fbf44ee2612827853a28d389845
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-06 14:33:02 -0700

    mem layout

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -241,14 +241,14 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
 
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
-  // struct proc *p = myproc();
-  // for(int i = 0; i < MAXTHREADS; i++){
-  //   if(p->thread[i].trapframe){
-  //     uint64 va = TRAPFRAME(i);
-  //     uvmunmap(pagetable, va, 1, 1); 
-  //     p->thread[i].trapframe=0;
-  //   }
-  // }
+  struct proc *p = myproc();
+  for(int i = 0; i < MAXTHREADS; i++){
+    if(p->thread[i].trapframe){
+      uint64 va = TRAPFRAME(i);
+      uvmunmap(pagetable, va, 1, 1); 
+      p->thread[i].trapframe=0;
+    }
+  }
 
   uvmfree(pagetable, sz);
 }
