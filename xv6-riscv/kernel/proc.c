5e84f5472a3125943b90cf0edf6b3112fa8675bd
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-06 10:35:46 -0700

    bbased on slack commens checing uvmunmap

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -245,10 +245,10 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
 
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
-  for(int i = 0; i < MAXTHREADS; i++){
-    uint64 va = TRAPFRAME(i);
-    uvmunmap(pagetable, va, 1, 1); 
-  }
+  // for(int i = 0; i < MAXTHREADS; i++){
+  //   uint64 va = TRAPFRAME(i);
+  //   uvmunmap(pagetable, va, 1, 1); 
+  // }
 
   uvmfree(pagetable, sz);
 }
