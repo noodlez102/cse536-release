d8fac6377e52352ecebcf01d4d54ff22560e7b54
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-05 22:50:03 -0700

    went through each one and compared to og

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -129,7 +129,6 @@ usertrapret(void)
   
   // CSE 536: (Task 2.1.2) - Pass the address of the current thread's TRAPFRAME in s11
   p->thread->trapframe->s11=(uint64)p->thread->trapframe;
-  ((void (*)(uint64))trampoline_userret)(satp);
 }
 
 // interrupts and exceptions from kernel code go here via kernelvec,
