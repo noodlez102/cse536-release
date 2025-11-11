613b86c6664026fa310d31b363e31da0ba90d261
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-11 11:06:47 -0700

    got rid of kalloc conversion

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -152,11 +152,11 @@ found:
     p->thread[tid].tid = tid;
     p->thread[tid].state = UNUSED;
   
-    t->trapframe->kernel_trap  = (uint64)usertrapret;
+    p->thread[tid].trapframe->kernel_trap  = (uint64)usertrapret;
     // printf("spec is in alloproc %p\n", p->thread[0].trapframe->epc);
-    t->trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
-    t->trapframe->kernel_hartid = r_tp();
-    t->trapframe->kernel_sp = KSTACK(p->pid, tid) + PGSIZE;
+    p->thread[tid].trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
+    p->thread[tid].trapframe->kernel_hartid = r_tp();
+    p->thread[tid].trapframe->kernel_sp = KSTACK(p->pid, tid) + PGSIZE;
     // p->thread[tid].trapframe->epc = p->thread[0].trapframe->epc;
     if((p->thread[tid].trapframe =((struct trapframe*)kalloc()))== 0) {
       freeproc(p);
