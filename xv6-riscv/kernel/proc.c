eec2fa2a8b525f4814d604ab38c96a94d56a6806
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-10 23:40:58 -0700

    writing stvec plus print statements

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -164,7 +164,7 @@ found:
   t->tid = 0; 
 
   t->trapframe->kernel_trap  = (uint64)usertrapret;
-  printf("spec is in alloproc %p\n", p->thread[0].trapframe->epc);
+  // printf("spec is in alloproc %p\n", p->thread[0].trapframe->epc);
   t->trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
   t->trapframe->kernel_hartid = r_tp();
   t->trapframe->kernel_sp = KSTACK(p->pid, 0) + PGSIZE;
