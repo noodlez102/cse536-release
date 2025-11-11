b4073dfc69c5ea298922e70e4b9923c28a6eaa10
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-10 22:46:33 -0700

    writing stvec

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -130,6 +130,7 @@ usertrapret(void)
   // CSE 536: (Task 2.1.2) - Pass the address of the current thread's TRAPFRAME in s11
   // mythread()->trapframe->s11 = TRAPFRAME(mythread()->tid);
   printf("sepc is %p", r_sepc());
+  printf("s11 is %p", TRAPFRAME(mythread()->tid));
   asm volatile("mv s11, %0" :: "r"(TRAPFRAME(mythread()->tid)) : "s11");
   ((void (*)(uint64))trampoline_userret)(satp);
 }
