cb455c090a930765ce4fad383fd24cede335fb08
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-11 11:05:11 -0700

    got rid of kalloc conversion

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -138,8 +138,6 @@ exec(char *path, char **argv)
  uint64 stacks_base = p->sz - (uint64)MAXTHREADS * 2 * PGSIZE;
 
   for (int i = 0; i < MAXTHREADS; i++) {
-    p->thread[i].trapframe->epc = elf.entry;  // initial program counter = main
-
     uint64 stack_page = stacks_base + (uint64)(i * 2 + 1) * PGSIZE;
     uint64 stack_top  = stack_page + PGSIZE;
     p->thread[i].trapframe->sp = stack_top;
