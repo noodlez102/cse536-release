dd2cacc015f94f1fb6b0cc2b83c76d8ca7b3e69d
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-10 21:16:45 -0700

    checking pec

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -164,6 +164,7 @@ found:
 
   t->trapframe->kernel_trap  = (uint64)usertrapret;
   w_stvec((uint64)usertrapret);
+  mythread()->trapframe->epc=r_sepc();
   t->trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
   t->trapframe->kernel_hartid = r_tp();
   t->trapframe->kernel_sp = KSTACK(p->pid, 0) + PGSIZE;
