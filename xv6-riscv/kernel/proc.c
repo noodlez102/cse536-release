bd098101c6c46b166fb8d3282d3f78b2b196be7d
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-11 10:17:58 -0700

    checking

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -163,11 +163,11 @@ found:
   t->state = USED;
   t->tid = 0; 
 
-  t->trapframe->kernel_trap  = (uint64)usertrapret;
-  // printf("spec is in alloproc %p\n", p->thread[0].trapframe->epc);
-  t->trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
-  t->trapframe->kernel_hartid = r_tp();
-  t->trapframe->kernel_sp = KSTACK(p->pid, 0) + PGSIZE;
+  // // t->trapframe->kernel_trap  = (uint64)usertrapret;
+  // // printf("spec is in alloproc %p\n", p->thread[0].trapframe->epc);
+  // t->trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
+  // t->trapframe->kernel_hartid = r_tp();
+  // t->trapframe->kernel_sp = KSTACK(p->pid, 0) + PGSIZE;
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
