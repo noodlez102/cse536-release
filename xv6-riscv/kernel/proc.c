d7653c97ce2969b22e47b728bdb15dd742657316
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-11 17:30:42 -0700

    got rid of the kernel trapfram

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -151,13 +151,11 @@ found:
   for(int tid = 0; tid < MAXTHREADS; tid++) {
     p->thread[tid].tid = tid;
     p->thread[tid].state = UNUSED;
-  
-    p->thread[tid].trapframe->kernel_satp  = MAKE_SATP(p->pagetable);
-    p->thread[tid].trapframe->kernel_hartid = r_tp();
-    p->thread[tid].trapframe->kernel_sp = KSTACK(p->pid, tid) + PGSIZE;
-    // p->thread[tid].trapframe->kernel_trap = (uint64)usertrap;
-    p->thread[tid].trapframe->epc = p->thread[0].trapframe->epc;
-
+    p->thread[tid].priority = 0;
+    p->thread[tid].chan = 0;
+    initlock(&p->thread[tid].lock, "thread");
+    memset(&p->thread[tid].context, 0, sizeof(p->thread[tid].context));
+    
     if((p->thread[tid].trapframe =((struct trapframe*)kalloc()))== 0) {
       freeproc(p);
       release(&p->lock);
