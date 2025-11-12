d5cbdc5ba9dfbbd140da33ecbab4e84020758f56
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-11 17:31:47 -0700

    got rid of the kernel trapfram

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -153,9 +153,9 @@ found:
     p->thread[tid].state = UNUSED;
     p->thread[tid].priority = 0;
     p->thread[tid].chan = 0;
-    initlock(&p->thread[tid].lock, "thread");
+    // initlock(&p->thread[tid].lock, "thread");
     memset(&p->thread[tid].context, 0, sizeof(p->thread[tid].context));
-    
+
     if((p->thread[tid].trapframe =((struct trapframe*)kalloc()))== 0) {
       freeproc(p);
       release(&p->lock);
