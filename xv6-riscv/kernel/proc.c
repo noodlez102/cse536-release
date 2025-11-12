86643ac306a6b00ae6945cbb4273bc54cb97e723
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 02:23:01 -0700

    initialized contest

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -43,7 +43,7 @@ proc_mapstacks(pagetable_t kpgtbl)
       char *pa = kalloc();
       if (pa == 0)
         panic("proc_mapstacks: kalloc");
-      // memset(pa, 0, PGSIZE);
+      memset(pa, 0, PGSIZE);
       uint64 va = KSTACK((int) (p - proc), tid);
       kvmmap(kpgtbl, va, (uint64)pa, PGSIZE, PTE_R | PTE_W | PTE_U);
     }
