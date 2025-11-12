785b47a62ac80a2a0d098c1400307145f147915c
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 11:46:53 -0700

    if

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -158,11 +158,12 @@ found:
     p->thread[tid].chan = 0;
     printf("this is what sepc is here %p\n", r_sepc);
     if((p->thread[tid].trapframe->epc= r_sepc())== 0) {
+      printf("hit the pthread epc if");
       freeproc(p);
       release(&p->lock);
       return 0;
     }
-    p->thread[tid].trapframe->epc= r_sepc();
+    // p->thread[tid].trapframe->epc= r_sepc();
     if((p->thread[tid].trapframe =((struct trapframe*)kalloc()))== 0) {
       freeproc(p);
       release(&p->lock);
