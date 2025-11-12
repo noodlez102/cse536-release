25cc099405d9268af93ab775e89baf9cc6cfd619
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 12:18:50 -0700

    not getting there

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -59,7 +59,7 @@ usertrap(void)
     // sepc points to the ecall instruction,
     // but we want to return to the next instruction.
     mythread()->trapframe->epc += 4;
-
+    printf("does this even get here?\n");
     // an interrupt will change sepc, scause, and sstatus,
     // so enable only now that we're done with those registers.
     intr_on();
