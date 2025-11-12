c21b6460b373f7c3595d1b824d53d21cd0a535bd
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 15:47:31 -0700

    please

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -43,7 +43,7 @@ proc_mapstacks(pagetable_t kpgtbl)
       char *pa = kalloc();
       if (pa == 0)
         panic("proc_mapstacks: kalloc");
-      // memset(pa, 0, PGSIZE);
+      memset(pa, 0, PGSIZE);
       uint64 va = KSTACK((int) (p - proc), tid);
       kvmmap(kpgtbl, va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
     }
@@ -239,7 +239,7 @@ proc_pagetable(struct proc *p)
   // printf("entering proc_pagetable\n");
 
   for(int i = 0; i < MAXTHREADS; i++) {
-    if (mappages(pagetable, TRAPFRAME(p->thread[i].tid), PGSIZE, (uint64)p->thread[i].trapframe, PTE_R | PTE_W | PTE_U) < 0) {
+    if (mappages(pagetable, TRAPFRAME(p->thread[i].tid), PGSIZE, (uint64)p->thread[i].trapframe, PTE_R | PTE_W ) < 0) {
       uvmunmap(pagetable, TRAMPOLINE, 1, 0);  
       uvmfree(pagetable, 0);
       return 0;
