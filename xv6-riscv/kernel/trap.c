7f0cb57ca8d724cec8bc69d0c6969e9342de2ac8
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 10:50:47 -0700

    went back

--- xv6-riscv/kernel/trap.c
+++ xv6-riscv/kernel/trap.c
@@ -131,7 +131,7 @@ usertrapret(void)
   
   // CSE 536: (Task 2.1.2) - Pass the address of the current thread's TRAPFRAME in s11
   mythread()->trapframe->s11 = TRAPFRAME(mythread()->tid);
-  printf("mythread()->tid = %d, thread[0].trapframe->epc = %p\n", mythread()->tid, p->thread[0].trapframe->epc);
+  printf("mythread()->tid = %d, thread[0].trapframe->epc = %p, trapframe->s11 = %p\n", mythread()->tid, p->thread[0].trapframe->epc, TRAPFRAME(mythread()->tid));
   asm volatile("mv s11, %0" :: "r"(TRAPFRAME(mythread()->tid)) : "s11");
   ((void (*)(uint64))trampoline_userret)(satp);
 }
