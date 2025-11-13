8c9d9bbee105d8048cc7a12042cf801691f00eb6
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 17:42:44 -0700

    stack

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -153,10 +153,7 @@ exec(char *path, char **argv)
   for (int i = 0; i < MAXTHREADS; i++) {
     uint64 stack_page = stacks_base + (uint64)(i * 2 + 1) * PGSIZE;
     uint64 stack_top  = stack_page + PGSIZE;
-    p->thread[i].trapframe->epc = elf.entry;
     p->thread[i].trapframe->sp = stack_top;
-    p->thread[i].tid = i;
-    p->thread[i].trapframe->s11=TRAPFRAME(i);
   }
 
   proc_freepagetable(oldpagetable, oldsz);
