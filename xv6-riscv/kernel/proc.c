cf75413f3c6e7c3aaf5b11f6659428c60337d93c
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 22:47:57 -0700

    changes

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -253,25 +253,25 @@ proc_pagetable(struct proc *p)
 void
 proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
-  struct proc *p = 0;
-  for (struct proc *pp = proc; pp < &proc[NPROC]; pp++) {
-    if (pp->pagetable == pagetable) {
-      p = pp;
-      break;
-    }
-  }
-  uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+  // struct proc *p = 0;
+  // for (struct proc *pp = proc; pp < &proc[NPROC]; pp++) {
+  //   if (pp->pagetable == pagetable) {
+  //     p = pp;
+  //     break;
+  //   }
+  // }
+  // uvmunmap(pagetable, TRAMPOLINE, 1, 0);
 
-  // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
-  // printf("entering proc_freepagetable\n");
-  // struct proc *p = myproc();
-  for(int i = 0; i < MAXTHREADS; i++){
-    uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
-    p->thread[i].trapframe = 0;    
-    p->thread[i].state = UNUSED;   
-  }
-  // printf("exiting proc_freepagetable\n");
-  uvmfree(pagetable, sz); 
+  // // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
+  // // printf("entering proc_freepagetable\n");
+  // // struct proc *p = myproc();
+  // for(int i = 0; i < MAXTHREADS; i++){
+  //   uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
+  //   p->thread[i].trapframe = 0;    
+  //   p->thread[i].state = UNUSED;   
+  // }
+  // // printf("exiting proc_freepagetable\n");
+  // uvmfree(pagetable, sz); 
 }
 
 // a user program that calls exec("/init")
