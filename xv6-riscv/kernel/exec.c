2c789f0cba0576ae4844b41bd9d909a270bf6691
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 21:44:07 -0700

    why does calling proc there cause errors

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -158,9 +158,7 @@ exec(char *path, char **argv)
     // printf("this is what the sepc value is in %d %p\n",i,p->thread[i].trapframe->epc);
     p->thread[i].trapframe->s11 = TRAPFRAME(i);
   }
-  p->thread[0].trapframe->sp = sz;
-  p->thread[0].trapframe->a1 = sp;
-  p->thread[0].trapframe->s11 = TRAPFRAME(0);
+
   proc_freepagetable(oldpagetable, oldsz);
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
