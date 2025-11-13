314e75cca766e8c427a7086de312c87e0fc5744e
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 19:49:50 -0700

    i dunno

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -158,7 +158,7 @@ exec(char *path, char **argv)
     p->thread[i].trapframe->s11 = TRAPFRAME(i);
   }
 
-  proc_freepagetable(oldpagetable, oldsz);
+  //proc_freepagetable(oldpagetable, oldsz);
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
