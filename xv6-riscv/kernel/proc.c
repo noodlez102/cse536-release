ef5a88fb510cd8e8910594332ca50eb789445370
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 20:46:04 -0700

    freeproc

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -260,12 +260,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
   // printf("entering proc_freepagetable\n");
 
-  struct proc *p = myproc();
   for(int i = 0; i < MAXTHREADS; i++){
-    if(p->thread[i].trapframe){
-      uint64 va = TRAPFRAME(i);
-      uvmunmap(pagetable, va, 1, 0); 
-    }
+    uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
+
   }
   // printf("exiting proc_freepagetable\n");
   uvmfree(pagetable, sz); 
