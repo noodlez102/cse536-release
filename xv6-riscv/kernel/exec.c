caf4c04dd27f10d107120073aefe79c4c5a916cf
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 19:52:09 -0700

    i dunno

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -141,7 +141,7 @@ exec(char *path, char **argv)
   safestrcpy(p->name, last, sizeof(p->name));
     
   // Commit to the user image.
-  // oldpagetable = p->pagetable;
+  oldpagetable = p->pagetable;
   p->pagetable = pagetable;
   p->sz = sz;
   p->thread[0].trapframe->epc = elf.entry;  // initial program counter = main
