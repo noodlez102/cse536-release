465a937ddbd096930311e4f8b7419e10348ac025
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 20:54:30 -0700

    got rid of freepageable again

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -255,16 +255,16 @@ proc_pagetable(struct proc *p)
 void
 proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
-  uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+  // uvmunmap(pagetable, TRAMPOLINE, 1, 0);
 
-  // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
-  // printf("entering proc_freepagetable\n");
+  // // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
+  // // printf("entering proc_freepagetable\n");
 
-  for(int i = 0; i < MAXTHREADS; i++){
-    uvmunmap(pagetable, TRAPFRAME(i), 1, 1);     
-  }
-  // printf("exiting proc_freepagetable\n");
-  uvmfree(pagetable, sz); 
+  // for(int i = 0; i < MAXTHREADS; i++){
+  //   uvmunmap(pagetable, TRAPFRAME(i), 1, 1);     
+  // }
+  // // printf("exiting proc_freepagetable\n");
+  // uvmfree(pagetable, sz); 
 }
 
 // a user program that calls exec("/init")
