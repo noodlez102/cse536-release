3e22aa8d0af51eac33586eda6fa941d7c359d873
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 23:34:47 -0700

    it boots a little

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -267,8 +267,8 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   // struct proc *p = myproc();
   for(int i = 0; i < MAXTHREADS; i++){
     uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
-    // p->thread[i].trapframe = 0;    
-    // p->thread[i].state = UNUSED;   
+    p->thread[i].trapframe = 0;    
+    p->thread[i].state = UNUSED;   
   }
   // printf("exiting proc_freepagetable\n");
   uvmfree(pagetable, sz); 
