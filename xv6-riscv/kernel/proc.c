398ca5f03fc1a0cf736c146225e87eb99bb1303b
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 21:19:18 -0700

    why does calling proc there cause errors

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -166,8 +166,6 @@ found:
     memset(p->thread[tid].trapframe, 0, PGSIZE);
   }
   // printf("exiting allocproc\n");
-  t = &p->thread[0];
-  t->state = USED;
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -240,7 +238,7 @@ proc_pagetable(struct proc *p)
 
   for(int i = 0; i < MAXTHREADS; i++) {
     if (mappages(pagetable, TRAPFRAME(p->thread[i].tid), PGSIZE, (uint64)p->thread[i].trapframe, PTE_R | PTE_W ) < 0) {
-      uvmunmap(pagetable, TRAMPOLINE, 1, 1);  
+      uvmunmap(pagetable, TRAMPOLINE, 1, 0);  
       uvmfree(pagetable, 0);
       return 0;
     }
