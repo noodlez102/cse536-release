efe74b0bc16cfa7dc018ced1c4eaff892b879a14
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 21:53:56 -0700

    why does calling proc there cause errors

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -44,7 +44,7 @@ proc_mapstacks(pagetable_t kpgtbl)
       if (pa == 0)
         panic("proc_mapstacks: kalloc");
       memset(pa, 0, PGSIZE);
-      uint64 va = KSTACK((int) (p - proc), tid) + PGSIZE;
+      uint64 va = KSTACK((int) (p - proc), tid) ;
       kvmmap(kpgtbl, va, (uint64)pa, PGSIZE, PTE_R | PTE_W);
     }
   }
