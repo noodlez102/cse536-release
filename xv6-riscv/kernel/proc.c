3275dad28c3fe85d26d589dc60cfa5f9963c18a3
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 20:58:46 -0700

    why does calling proc there cause errors

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -260,10 +260,10 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
   // printf("entering proc_freepagetable\n");
   // struct proc *p = myproc();
-  for(int i = 0; i < MAXTHREADS; i++){
-    uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
-    // p->thread[i].trapframe=0;
-  }
+  // for(int i = 0; i < MAXTHREADS; i++){
+  //   uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
+  //   // p->thread[i].trapframe=0;
+  // }
   // printf("exiting proc_freepagetable\n");
   uvmfree(pagetable, sz); 
 }
