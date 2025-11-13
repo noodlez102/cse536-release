45303d38eeef3b9efc8aaa4efdacbd6dc60d4d78
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 18:10:29 -0700

    more pritns

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -22,7 +22,7 @@ int flags2perm(int flags)
 int
 exec(char *path, char **argv)
 {
-  printf("entering exec\n");
+  // printf("entering exec\n");
   char *s, *last;
   int i, off;
   uint64 argc, sz = 0, sp, ustack[MAXARG], stackbase;
