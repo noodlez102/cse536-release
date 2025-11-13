87246ab0e13858d8b00eae945b87a59a19f27a19
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-12 22:47:14 -0700

    changes

--- xv6-riscv/kernel/memlayout.h
+++ xv6-riscv/kernel/memlayout.h
@@ -54,7 +54,7 @@
 // map kernel stacks beneath the trampoline,
 // each surrounded by invalid guard pages.
 // CSE 536: (Task 2.1.1): Update this macro to accept (pid, tid) and return a virtual address according to layout provided in the assignment instructions.
-#define KSTACK(p,tid) (TRAMPOLINE - ((p)*MAXTHREADS + (tid) + 1) * 2 * PGSIZE + PGSIZE)
+#define KSTACK(p,tid) (TRAMPOLINE - ((p)*MAXTHREADS + (tid) + 1) * 2 * PGSIZE )
 
 // User memory layout.
 // Address zero first:
