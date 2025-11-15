9a65bd85840c07f3e743e9f58b771cd12ea1783a
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-14 23:49:01 -0700

    new maxthread userstracks

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -179,7 +179,9 @@ exec(char *path, char **argv)
     // uint64 stack_top  = stack_page + PGSIZE;
     // uint64 stack_top_2 = p->sz - (i * 2 * PGSIZE);
     // printf("%p the stack top is , and the stack top 2 is %p\n", stack_top, stack_top_2);
-    p->thread[i].trapframe->sp = sp;
+    uint64 stack_top = p->sz - (i * 2 * PGSIZE);
+    p->thread[i].trapframe->sp = stack_top;
+    p->thread[i].trapframe->s11 = TRAPFRAME(i);
     // p->thread[i].trapframe->epc = elf.entry;
     // printf("this is what the sepc value is in %d %p\n",i,p->thread[i].trapframe->epc);
     // p->thread[i].trapframe->s11 = TRAPFRAME(i);
