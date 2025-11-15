cc067e9252882e4346f7900c1712b6f194d1df96
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-14 23:09:41 -0700

    hmm

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -120,7 +120,7 @@ exec(char *path, char **argv)
   // uint64 personalOldsz=0;
   for (int i=0; i< MAXTHREADS;i++){
     uint64 sz1;
-    if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE, PTE_W | PTE_U)) == 0)
+    if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE, PTE_W | PTE_U | PTE_R)) == 0)
       goto bad;
     sz = sz1;
     uvmclear(pagetable, sz-2*PGSIZE);
