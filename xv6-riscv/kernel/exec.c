c9a7523a6f50fa3748115c22053005760ca6f4f7
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-14 23:15:53 -0700

    hmm

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -118,7 +118,7 @@ exec(char *path, char **argv)
 
   //writing bar for bar of old xv6 code
   // uint64 personalOldsz=0;
-  // for (int i=0; i< MAXTHREADS;i++){
+  for (int i=0; i< MAXTHREADS;i++){
     uint64 sz1;
     if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE, PTE_W | PTE_U | PTE_R)) == 0)
       goto bad;
@@ -127,7 +127,7 @@ exec(char *path, char **argv)
     // if(i==0){
     //   personalOldsz=sz;
     // }
-  // }
+  }
   // sz=personalOldsz;
   sp = sz;
   stackbase = sp - PGSIZE;
@@ -174,20 +174,19 @@ exec(char *path, char **argv)
   // CSE 536: (Task 2.1.1) - set the correct user stack in the in each thread's trapframe
   // uint64 stacks_base = p->sz - (uint64)MAXTHREADS * 2 * PGSIZE;
   // printf("comparison vs mine and other stack base %p: mine, %p: theirs\n",stacks_base, stackbase);
-  // for (int i = 0; i < MAXTHREADS; i++) {
+  for (int i = 0; i < MAXTHREADS; i++) {
     // uint64 stack_page = stackbase + (uint64)(i * 2 + 1) * PGSIZE;
   
     // uint64 stack_top  = stack_page + PGSIZE;
     // uint64 stack_top_2 = p->sz - (i * 2 * PGSIZE);
     // printf("%p the stack top is , and the stack top 2 is %p\n", stack_top, stack_top_2);
-    p->thread[0].trapframe->sp = sp;
+    p->thread[i].trapframe->sp = sp;
     // p->thread[i].trapframe->epc = elf.entry;
     // printf("this is what the sepc value is in %d %p\n",i,p->thread[i].trapframe->epc);
     // p->thread[i].trapframe->s11 = TRAPFRAME(i);
-  // }
-  procdump();
+  }
+
   proc_freepagetable(oldpagetable, oldsz);
-  procdump();
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
