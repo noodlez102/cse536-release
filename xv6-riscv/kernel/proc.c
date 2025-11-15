937333d84d63a9ddd0813b73ceb5bf28563be43b
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-14 23:11:26 -0700

    hmm

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -266,11 +266,11 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
   // printf("entering proc_freepagetable\n");
-  for(int i = 0; i < MAXTHREADS; i++){
-    uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
+  // for(int i = 0; i < MAXTHREADS; i++){
+    uvmunmap(pagetable, TRAPFRAME(0), 1, 1); 
     // p->thread[i].trapframe = 0;    
     // p->thread[i].state = UNUSED;   
-  }
+  // }
   // printf("exiting proc_freepagetable\n");
   uvmfree(pagetable, sz); 
 }
