277e6c71f156f06503da1182802187f23a6ae6c3
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-14 21:18:00 -0700

    please

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -120,7 +120,7 @@ exec(char *path, char **argv)
 
   for (int i=0; i< MAXTHREADS;i++){
     uint64 sz1;
-    if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE, PTE_W )) == 0)
+    if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE, PTE_W | PTE_R | PTE_U)) == 0)
       goto bad;
     sz = sz1;
     uvmclear(pagetable, sz-2*PGSIZE);
--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -267,7 +267,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
   // printf("entering proc_freepagetable\n");
   for(int i = 0; i < MAXTHREADS; i++){
-    uvmunmap(pagetable, TRAPFRAME(i), 2, 1); 
+    uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
     // p->thread[i].trapframe = 0;    
     // p->thread[i].state = UNUSED;   
   }
