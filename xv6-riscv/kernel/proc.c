705f25fe11a4585a740d67ba68600603a621f309
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-18 15:49:01 -0700

    test

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -163,7 +163,7 @@ found:
       return 0;
     }
     // p->thread[tid].trapframe->epc = r_sepc();
-    // memset(p->thread[tid].trapframe, 0, PGSIZE);
+    memset(p->thread[tid].trapframe, 0, PGSIZE);
   }
   // printf("exiting allocproc\n");
 
@@ -267,7 +267,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   // CSE 536: (Task 2.1.1) - unmap and free all the trapframes
   // printf("entering proc_freepagetable\n");
   for(int i=0; i<MAXTHREADS;i++){
-    uvmunmap(pagetable, TRAPFRAME(i), 2, 1); 
+    uvmunmap(pagetable, TRAPFRAME(i), 1, 1); 
   }
     // p->thread[i].trapframe = 0;    
     // p->thread[i].state = UNUSED;   
