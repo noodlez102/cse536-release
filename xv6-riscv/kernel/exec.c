6c5b12c840b90c29e68019568021214c98cd20c9
Arthur Hua <arthur.hua.102@gmail.com>
2025-11-18 15:50:59 -0700

    test

--- xv6-riscv/kernel/exec.c
+++ xv6-riscv/kernel/exec.c
@@ -119,7 +119,7 @@ exec(char *path, char **argv)
   //writing bar for bar of old xv6 code
   // uint64 personalOldsz=0;
   uint64 sz1;
-  if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE*MAXTHREADS, PTE_W )) == 0)
+  if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE*MAXTHREADS, PTE_W | PTE_U)) == 0)
     goto bad;
   sz = sz1;
   for (int i=0; i< MAXTHREADS;i++){
